5e1b85e136fe9132356a84312005e91f92f4a764
Santiago Sosa <santisosalp@gmail.com>
2024-03-13 10:11:48 -0300

    hola

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./src/**/*.{html,js}",
+  content: ["./index.html", "./pages/descubre.html", "./pages/contacto.html", "./src/**/*.{html,js}",
   "./node_modules/flowbite/**/*.js"
 ],
   theme: {
